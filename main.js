f5fe9f390502bf7012dff600e9dfabb4b148f038
ADEBIYIAYOMIDE <temiayomi99@gmail.com>
2024-09-19 10:18:23 +0100

    logo

--- main.js
+++ main.js
@@ -1,64 +1,64 @@
-function myHeader(){
+function myHeader() {
     let header = document.getElementById("header");
     let lol = document.getElementById("lol");
-    window.addEventListener("scroll" , function(){
-        if(window.scrollY > 0){
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 0) {
             header.classList.add("active");
             lol.src = "pic/bulb2.png";
-        }else{
+        } else {
             header.classList.remove("active");
             lol.src = "pic/bulb.png";
         }
-        
+
     })
 }
 myHeader();
 
-function myBars(){
+function myBars() {
     let bars = document.querySelector("#bar");
     let nav = document.querySelector(".navigation");
-    bars.onclick = function(){
-        if(nav.style.right == "0%"){
+    bars.onclick = function () {
+        if (nav.style.right == "0%") {
             nav.style.right = "-50%";
             bars.src = "pic/menu.png"
-        }else{
+        } else {
             nav.style.right = "0%";
             bars.src = "pic/x.png"
         }
         nav.classList.toggle("new")
     }
-    
+
 }
 myBars()
 
-function myFun(){
+function myFun() {
     let plus = document.querySelector(".plus");
     let textBox = document.querySelector(".text-box ");
-    plus.onclick = ()=>{
+    plus.onclick = () => {
         textBox.classList.toggle("active");
         plus.classList.toggle("img_active")
     }
 }
 myFun()
 
-function myVideo(){
+function myVideo() {
     let links = document.querySelector(".link_a");
     let overs = document.querySelector(".bg-show .overlay");
     let exit = document.querySelector(".cancel");
     let player = document.querySelector(".player");
     let videos = document.getElementById("video");
-    links.onclick = (ed)=>{
+    links.onclick = (ed) => {
         ed.preventDefault();
         overs.style.display = "block"
     }
-    exit.onclick = ()=>{
+    exit.onclick = () => {
         overs.style.display = "none"
     }
-    player.onclick = ()=>{
-        if(videos.paused){
+    player.onclick = () => {
+        if (videos.paused) {
             videos.play();
             player.src = "pic/pause.png"
-        }else{
+        } else {
             videos.pause();
             player.src = "pic/play.png"
         }
@@ -66,36 +66,19 @@ function myVideo(){
 }
 myVideo();
 
-function toTop(){
+function toTop() {
     let top = document.querySelector(".top");
-    window.onscroll = ()=>{
-        if(window.scrollY > 100 || document.documentElement.scrollTop > 100){
+    window.onscroll = () => {
+        if (window.scrollY > 100 || document.documentElement.scrollTop > 100) {
             top.style.display = "block"
-        }else{
+        } else {
             top.style.display = "none"
         }
     }
 
-    top.onclick = ()=>{
-        scrollTo(0,0)
+    top.onclick = () => {
+        scrollTo(0, 0)
     }
 
 }
 toTop();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
